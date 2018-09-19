c9d58f161fad151659d722ef5b0d4a0480bdb9f8
hando <enthusapp@gmail.com>
2018-09-19 09:37:38 +0900

    run backup program when disconnected

--- vncmonitor.js
+++ vncmonitor.js
@@ -4,6 +4,9 @@ const hd = require('./lib/helper');
 const config = {
   ip: '192.168.0.109',
   port: '5900',
+  process2: {
+    command: 'python',
+    option: ['/home/pi/Videos/mf_rtp.py']},
   firstTime: 1,
   failWaitTime: -540,
   recoverTime: 10
@@ -13,29 +16,45 @@ let vnc = {
   count: 0,
   first: true,
   connected: false,
-  process: null
+  process1: null,
+  process2: null
 };
 
-const vncRun = () => {
+function vncRun() {
   console.log('vncRun');
-  vnc.process = hd.child('vncviewer', [config.ip], {
+  vnc.connected = true;
+  vnc.process1 = hd.child('vncviewer', [config.ip], {
     stdout: () => {},
     stderr: () => {},
     close: () => {
       vnc.connected = false;
-      vnc.process = null;
+      vnc.process1 = null;
     },
   })();
 };
 
-const vncStop = () => {
+function vncStop() {
   console.log('vncStop');
-  if (vnc.process != null)
-    vnc.process.kill();
+  if (vnc.process1 != null)
+    vnc.process1.kill();
 };
 
-const localRun = () => {console.log('localRun');};
-const localStop = () => {console.log('localStop');};
+function localRun() {
+  console.log('localRun');
+  vnc.process2 = hd.child(config.process2.command, config.process2.option, {
+    stdout: () => {},
+    stderr: () => {},
+    close: () => {
+      vnc.process2 = null;
+    },
+  })();
+};
+
+function localStop() {
+  console.log('localStop');
+  if (vnc.process2 != null)
+    vnc.process2.kill();
+};
 
 setInterval(hd.childErrorOnce('nc', ['-vz', config.ip, config.port], data => {
   if (/succeeded/.test(`${data}`)) {
@@ -47,13 +66,12 @@ setInterval(hd.childErrorOnce('nc', ['-vz', config.ip, config.port], data => {
   if (vnc.first) {
       if (vnc.count > config.firstTime) {
         vnc.first = false;
-        vnc.connected = true;
         vncRun();
       }
  } else {
     if (vnc.connected) {
-    /* for test
-      if (vnc.count > 100) {
+      /* for test
+      if (vnc.count > 50) {
         vnc.count = 0;
         */
       if (vnc.count < config.failWaitTime) {
@@ -62,7 +80,6 @@ setInterval(hd.childErrorOnce('nc', ['-vz', config.ip, config.port], data => {
       }
     } else {
       if (vnc.count > config.recoverTime) {
-        vnc.connected = true;
         vncRun();
         localStop(); 
       }
